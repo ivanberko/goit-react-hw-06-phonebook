99c3aa4c74be691291702049c1349d40123d8527
Ivan Berk <berkoivan@gmail.com>
2020-06-15 18:53:08 +0300

    Updates

---
+++ precache-manifest.74ba2e8e0c047b53d4af1b5b52ba30a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beab5e75db6223097aaa0700f22ad1d0",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "ab87af7d99bcb090eabc",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.8141016c.chunk.css"
+  },
+  {
+    "revision": "0a8215d1a4f46433d0ce",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.88729640.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.88729640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab87af7d99bcb090eabc",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.825476da.chunk.js"
+  },
+  {
+    "revision": "86e9a7fe0c1479afab58",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.2d3ded43.js"
+  }
+]);
